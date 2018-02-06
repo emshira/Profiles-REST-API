a510dada3c1054dcb0f4c2f529fcb18034cb834a
Nashira Emery <nashiraemery1@gmail.com>
2018-02-06 15:31:50 -0500

    edit models.py for super user

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -22,8 +22,8 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-        def create_superuser(self, email, name, password):
-            """Creates and saves a new superuser with given details."""
+    def create_superuser(self, email, name, password):
+        cd """Creates and saves a new superuser with given details."""
 
         user = self.create_user(email, name, password)
 
